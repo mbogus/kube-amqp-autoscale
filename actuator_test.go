9a6aac44a34f2359a569681af5250039156e9f6c
mbogus <mbogus@users.noreply.github.com>
2016-07-08 22:20:15 +0100

    Shallow clone Kubernetes repository

--- actuator_test.go
+++ actuator_test.go
@@ -36,7 +36,7 @@ func TestAutoscaleClosedChannel(t *testing.T) {
 	forever := make(chan struct{}, 1)
 	close(forever)
 	var err error
-	autoscale(func() (*queueMetrics, error) { err = errors.New("Shouldn't be here"); return nil, err }, &scaleContext{}, forever)
+	autoscale(func() (*queueMetrics, error) { return nil, errors.New("error") }, &scaleContext{}, forever)
 	if err != nil {
 		t.Fatal(err)
 	}
